f2dc5f930ae37769c0ab27afb5278c2616ef3758
wspy <wspy@example.com>
2026-01-07 20:37:23 +0700

    Add layout and styling with CSS, and refactor FT waits

--- mysite/functional_tests/tests.py
+++ mysite/functional_tests/tests.py
@@ -6,6 +6,10 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 from selenium.webdriver.common.by import By
 
+from selenium.common.exceptions import WebDriverException
+import time
+
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -22,10 +26,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+             try:
+                table = self.browser.find_element(By.ID, "id_list_table")  
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return  
+             
+             except (AssertionError, WebDriverException):  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise  
+                time.sleep(0.5)
+
 
     def test_can_start_a_todo_list(self):
         # เปลี่ยน('http://localhost:8000') เป็นliveserver
@@ -43,20 +57,18 @@ class NewVisitorTest(LiveServerTestCase):
             'Enter a to-do item'
         )
 
-        import time
 
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         inputbox = self.browser.find_element("id", "id_new_item") 
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+    
 
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         table = self.browser.find_element("id", "id_list_table")
         rows = table.find_elements("tag name", "tr")
